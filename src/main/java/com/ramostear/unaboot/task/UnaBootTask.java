7100847ed5cd4baf06b749bd76fadb56289fc1fa
ramostear <ramostear@163.com>
2020-04-07 09:12:02 +0800

    1.Fix timed task of clean system cache 2.Fix the missing visits data error when updating articles

--- src/main/java/com/ramostear/unaboot/task/UnaBootTask.java
+++ src/main/java/com/ramostear/unaboot/task/UnaBootTask.java
@@ -7,9 +7,15 @@ import com.ramostear.unaboot.service.JobService;
 import com.ramostear.unaboot.service.LuceneService;
 import com.ramostear.unaboot.service.PostService;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName UnaBootTask
@@ -27,9 +33,8 @@ public class UnaBootTask {
     @Autowired
     private JobService jobService;
     @Autowired
-    private CronTaskRegister cronTaskRegister;
-    @Autowired
     private LuceneService luceneService;
+    private static CacheManager cacheManager = CacheManager.newInstance();
 
     /**
      * 定时发布文章
@@ -60,6 +65,39 @@ public class UnaBootTask {
      * 刷新系统缓存
      */
     public void clearCache(){
+        String[] cacheNames = cacheManager.getCacheNames();
+        if(cacheNames != null){
+            for(String cacheName : cacheNames){
+                Ehcache ehcache = cacheManager.getEhcache(cacheName);
+                if(cacheName.equals("dayHits")){
+                    clearCache(ehcache);
+                }
+                ehcache.removeAll();
+            }
+        }
+    }
 
+    private void clearCache(Ehcache ehcache){
+        List<String> keys = (List<String>)ehcache.getKeys();
+        List<Post> cachePosts = new ArrayList<>();
+        if(!CollectionUtils.isEmpty(keys)){
+            keys.forEach(key->{
+                Element element = ehcache.get(key);
+                Integer postId = Integer.parseInt(key.split("_")[0]);
+                long count = 0;
+                if(element != null){
+                    count = (Long) element.getObjectValue();
+                }
+                Post post = postService.findById(postId);
+                if(post != null){
+                    post.setVisits(post.getVisits()+count);
+                    cachePosts.add(post);
+                }
+
+            });
+        }
+        if(cachePosts.size() > 0){
+            postService.updateInBatch(cachePosts);
+        }
     }
 }
